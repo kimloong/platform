edaede3cb1c836d1fb66366260242440863da85a
张金龙 <150429@nd.com>
2017-08-22 18:25:37 +0800

    修改config配置

--- uaa/src/main/java/me/kimloong/uaa/UAAApplication.java
+++ uaa/src/main/java/me/kimloong/uaa/UAAApplication.java
@@ -1,16 +1,17 @@
-package me.kimloong.config;
+package me.kimloong.uaa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Created by Zhang JinLong(150429) on 2017-08-22.
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableConfigServer
-public class ConfigApplication {
+public class UAAApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+        SpringApplication.run(UAAApplication.class, args);
     }
 }
